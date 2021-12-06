2b9af85d1b9a8dca51c9d99496a4f9a8f255c799
VanesMP <vanessa.MP@outlook.fr>
2021-12-06 21:55:54 +0100

    nouveau fichier pour le html en js

---
+++ htmlTemplate.js
@@ -0,0 +1,87 @@
+//DOM
+const myRecipe = document.querySelector('.recipes');
+
+//création d un template pour la recette en html
+
+export function createElementHtml(recipe) {
+    let listIngredients = document.createElement('ul');
+    listIngredients.classList.add('listIngredients');
+    for (let i = 0; i < recipe.ingredients.length; i++) {
+        let myIngredient = document.createElement('li');
+        myIngredient.classList.add('myIngredient');
+        if (recipe.ingredients[i].quantity === undefined && recipe.ingredients[i].unit === undefined) {
+            myIngredient.innerHTML = recipe.ingredients[i].ingredient;
+        } else if (recipe.ingredients[i].quantity !== ' ' && recipe.ingredients[i].unit === undefined) {
+            myIngredient.innerHTML = recipe.ingredients[i].ingredient + ': ' + recipe.ingredients[i].quantity;
+        } else if (recipe.ingredients[i].quantity !== ' ' || recipe.ingredients[i].unit !== ' ') {
+            myIngredient.innerHTML = recipe.ingredients[i].ingredient + ': ' + recipe.ingredients[i].quantity + ' ' + recipe.ingredients[i].unit;
+        }
+        listIngredients.appendChild(myIngredient)
+    }
+    /*recipe.forEach((element) => {
+        let myIngredient = document.createElement('li');
+        myIngredient.classList.add('myIngredient');
+        if (element.ingredients.quantity === undefined && element.ingredients.unit === undefined) {
+            myIngredient.innerHTML = element.ingredients.ingredient;
+        } else if (element.ingredients.quantity !== ' ' && element.ingredients.unit === undefined) {
+            myIngredient.innerHTML = element.ingredients.ingredient + ': ' + element.ingredients.quantity;
+        } else if (element.ingredients.quantity !== ' ' || element.ingredients.unit !== ' ') {
+            myIngredient.innerHTML = element.ingredients.ingredient + ': ' + element.ingredients.quantity + ' ' + element.ingredients.unit;
+        }
+        listIngredients.appendChild(myIngredient)
+    });*/
+    //le conteneur de tous les elements necessaires pour la recette
+    const oneRecipe = document.createElement('div');
+    oneRecipe.classList.add('oneRecipe');
+    oneRecipe.setAttribute('id', recipe.id);
+    //Emplacement de la photo de la recette
+    const imageRecipe = document.createElement('div');
+    imageRecipe.classList.add('imageRecipe');
+    //Partie du bas de la recette, les textes
+    const textRecipe = document.createElement('div'); //1: le conteneur de tous les textes first et second
+    textRecipe.classList.add('textRecipe');
+    const insideTextRecipe = document.createElement('div');
+    insideTextRecipe.classList.add('insideTextRecipe');
+    //first partie : h1: name et elements time
+    const firstPartTextRecipe = document.createElement('div');
+    firstPartTextRecipe.classList.add('firstPartTextRecipe');
+    const nameRecipe = document.createElement('h1'); //h1 = nom de la recette
+    nameRecipe.classList.add('nameRecipe');
+    nameRecipe.innerHTML = recipe.name;
+    //conteneur des elements de time : icone + durée
+    const boxTime = document.createElement('div');
+    boxTime.classList.add('boxTime');
+    const timeIcone = document.createElement('div'); //icone horloge
+    timeIcone.classList.add('timeIcone');
+    const timeRecipe = document.createElement('div');
+    timeRecipe.classList.add('timeRecipe'); //durée de la recette + min
+    timeRecipe.innerHTML = recipe.time;
+    const minute = document.createElement('p');
+    minute.classList.add('timeRecipe');
+    minute.innerHTML = "min";
+    //second partie: liste des ingrédients + préparation de la recette
+    const secondPartTextRecipe = document.createElement('div');
+    secondPartTextRecipe.classList.add('secondPartTextRecipe');
+    //le texte de préparation de la recette
+    const preparationRecipe = document.createElement('div');
+    preparationRecipe.classList.add('preparationRecipe');
+    preparationRecipe.innerHTML = recipe.description;
+    //Methode appendChild
+    //inserer la premiere partie de la 1 : nom + durée
+    firstPartTextRecipe.appendChild(nameRecipe)
+    boxTime.appendChild(timeIcone)
+    boxTime.appendChild(timeRecipe)
+    boxTime.appendChild(minute)
+    firstPartTextRecipe.appendChild(boxTime)
+        //inserer la deuxieme partie de la 1 : 
+    secondPartTextRecipe.appendChild(listIngredients)
+    secondPartTextRecipe.appendChild(preparationRecipe)
+    insideTextRecipe.appendChild(firstPartTextRecipe)
+    insideTextRecipe.appendChild(secondPartTextRecipe)
+    textRecipe.appendChild(insideTextRecipe)
+        //inserer la partie image puis partie texte
+    oneRecipe.appendChild(imageRecipe)
+    oneRecipe.appendChild(textRecipe)
+        //appendChild le plus grand
+    myRecipe.appendChild(oneRecipe)
+}
--- index-script.js
+++ index-script.js
@@ -1,5 +1,7 @@
 import { recipes } from './recipes.js';
+import { createElementHtml } from './htmlTemplate.js';
 import { Chips } from './chips.js';
+
 
 //DOM
 let dataListIngredient = document.getElementById('listElementIngredient');
@@ -47,84 +49,14 @@ const APPAREIL = 'APPAREIL';
 
 //PARTIE 1 : implementation des recettes
 //Methode pourappeller la methode création du html
-function appelCreateHtmlRecette(recipe) {
-    for (let i = 0; i < recipe.length; i++) {
-        createElementHtml(recipe[i])
-    }
+function appelCreateHtmlRecette(recipes) {
+    recipes.forEach((recipe) => {
+        createElementHtml(recipe)
+    })
 }
 appelCreateHtmlRecette(recipes)
 
-//création d un template pour la recette en html
-function createElementHtml(recipe) {
-    let listIngredients = document.createElement('ul');
-    listIngredients.classList.add('listIngredients');
-    for (let i = 0; i < recipe.ingredients.length; i++) {
-        let myIngredient = document.createElement('li');
-        myIngredient.classList.add('myIngredient');
-        if (recipe.ingredients[i].quantity === undefined && recipe.ingredients[i].unit === undefined) {
-            myIngredient.innerHTML = recipe.ingredients[i].ingredient;
-        } else if (recipe.ingredients[i].quantity !== ' ' && recipe.ingredients[i].unit === undefined) {
-            myIngredient.innerHTML = recipe.ingredients[i].ingredient + ': ' + recipe.ingredients[i].quantity;
-        } else if (recipe.ingredients[i].quantity !== ' ' || recipe.ingredients[i].unit !== ' ') {
-            myIngredient.innerHTML = recipe.ingredients[i].ingredient + ': ' + recipe.ingredients[i].quantity + ' ' + recipe.ingredients[i].unit;
-        }
-        listIngredients.appendChild(myIngredient)
-    }
-    //le conteneur de tous les elements necessaires pour la recette
-    const oneRecipe = document.createElement('div');
-    oneRecipe.classList.add('oneRecipe');
-    oneRecipe.setAttribute('id', recipe.id);
-    //Emplacement de la photo de la recette
-    const imageRecipe = document.createElement('div');
-    imageRecipe.classList.add('imageRecipe');
-    //Partie du bas de la recette, les textes
-    const textRecipe = document.createElement('div'); //1: le conteneur de tous les textes first et second
-    textRecipe.classList.add('textRecipe');
-    const insideTextRecipe = document.createElement('div');
-    insideTextRecipe.classList.add('insideTextRecipe');
-    //first partie : h1: name et elements time
-    const firstPartTextRecipe = document.createElement('div');
-    firstPartTextRecipe.classList.add('firstPartTextRecipe');
-    const nameRecipe = document.createElement('h1'); //h1 = nom de la recette
-    nameRecipe.classList.add('nameRecipe');
-    nameRecipe.innerHTML = recipe.name;
-    //conteneur des elements de time : icone + durée
-    const boxTime = document.createElement('div');
-    boxTime.classList.add('boxTime');
-    const timeIcone = document.createElement('div'); //icone horloge
-    timeIcone.classList.add('timeIcone');
-    const timeRecipe = document.createElement('div');
-    timeRecipe.classList.add('timeRecipe'); //durée de la recette + min
-    timeRecipe.innerHTML = recipe.time;
-    const minute = document.createElement('p');
-    minute.classList.add('timeRecipe');
-    minute.innerHTML = "min";
-    //second partie: liste des ingrédients + préparation de la recette
-    const secondPartTextRecipe = document.createElement('div');
-    secondPartTextRecipe.classList.add('secondPartTextRecipe');
-    //le texte de préparation de la recette
-    const preparationRecipe = document.createElement('div');
-    preparationRecipe.classList.add('preparationRecipe');
-    preparationRecipe.innerHTML = recipe.description;
-    //Methode appendChild
-    //inserer la premiere partie de la 1 : nom + durée
-    firstPartTextRecipe.appendChild(nameRecipe)
-    boxTime.appendChild(timeIcone)
-    boxTime.appendChild(timeRecipe)
-    boxTime.appendChild(minute)
-    firstPartTextRecipe.appendChild(boxTime)
-        //inserer la deuxieme partie de la 1 : 
-    secondPartTextRecipe.appendChild(listIngredients)
-    secondPartTextRecipe.appendChild(preparationRecipe)
-    insideTextRecipe.appendChild(firstPartTextRecipe)
-    insideTextRecipe.appendChild(secondPartTextRecipe)
-    textRecipe.appendChild(insideTextRecipe)
-        //inserer la partie image puis partie texte
-    oneRecipe.appendChild(imageRecipe)
-    oneRecipe.appendChild(textRecipe)
-        //appendChild le plus grand
-    myRecipe.appendChild(oneRecipe)
-}
+
 
 //---------------------- TRI DE NAVIGATION : LA BARRE DE RECHERCHE GENERALE --------------------------
 //EventListener sur la loupe de la barre de menu principal pour faire le premier tri des recettes 
@@ -149,35 +81,25 @@ function firstSortRecipeByMainSearch() {
     if (userValueInMainSearch.length >= 3) {
         selectionRecipe = listRecipe.filter((element) => {
             if (element.name.toLowerCase().includes(userValueInMainSearch.toLowerCase())) {
-                //console.log('consition name: ', element.name)
                 return true
             }
             if (element.description.toLowerCase().includes(userValueInMainSearch.toLowerCase())) {
-                //console.log('consition description: ', element)
                 return true
             }
             let selectionByIngredient = element.ingredients.filter((el) => {
                 if (el.ingredient.toLowerCase().includes(userValueInMainSearch.toLowerCase())) {
-                    //console.log('consition ingredient: ', element)
                     return true
                 }
             })
             if (selectionByIngredient.length > 0) {
                 return true;
             }
-            /* for (i = 0; i < element.ingredient.length; i++) {
-                 if (element.ingredient[i].toLowerCase.includes(userValueInMainSearch.toLowerCase())) {
-                     return true;
-                 }
-             }*/
         });
         console.log(selectionRecipe)
         appelCreateHtmlRecette(selectionRecipe);
         //Ne laisser dansles dropdown que les data(ingredient & appareil & ustensile) correspond a ceux des recettes
         recupDataForDropdown(selectionRecipe);
-        //sortRecipeChips(selectionRecipe)
     }
-
     if (selectionRecipe == '') {
         inputMainSearch.value = '';
         inputMainSearch.placeholder = `Aucune recette ne correspond à votre critère… vous pouvez chercher «tarte aux pommes», «poisson» etc.`;
